436d110f17525746d693eccfd60babea4afc4db8
Leticia Kawamoto <leticiac.0401@gmail.com>
2023-07-12 10:19:29 -0300

    Update productService.js

--- productService.js
+++ productService.js
@@ -1,7 +1,10 @@
 async function getProduct(productId) {
-    const apiUrl = `https://fakestoreapi.com/products/${productId}`;
+  const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-    // Add your solution here!
+  const response = await fetch(apiUrl);
+	const jsonData = await response.json();
+	
+	return jsonData;
 }
 
 module.exports = getProduct;
